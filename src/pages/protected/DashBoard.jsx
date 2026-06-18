324a532661c93629dafd2c2c29cdd4be7210c730
Shanu <shanuvr112233@gmail.com>
2026-06-18 10:15:32 +0530

    feat: starting the dashboard page

---
+++ src/pages/protected/DashBoard.jsx
@@ -0,0 +1,11 @@
+
+import UserLayout from "../../../layouts/userlayout"
+function DashBoard() {
+  return (
+    <UserLayout>
+        <div>DashBoard</div>
+    </UserLayout>
+  )
+}
+
+export default DashBoard
